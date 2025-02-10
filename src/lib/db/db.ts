7e56a7830e3aab4efae97e049584851cecb1c916
er-vivekkhedkar <vivekkhedkar110@gmail.com>
2025-02-10 18:10:21 +0530

    Initial commit

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,17 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ['error', 'warn']  // Add logging for debugging
-  });
-};
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    },
+  },
+})
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
-
-export default prisma;
+export default prisma
